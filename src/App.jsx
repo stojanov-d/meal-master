b7d1c843c905a09458af3f0f732d430998c845e7
stojanov-d <stojanovdavid3@gmail.com>
2024-03-05 17:00:59 +0100

    Added Google Map component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import ToggleTheme from './components/ToggleTheme';
 import MainText from './components/MainText';
 import SearchBar from './components/SearchBar';
 import { Box } from '@chakra-ui/react';
+import MemoizedMainMap from './components/MainMap';
 function App() {
 	return (
 		<Box p={7}>
 			<ToggleTheme />
 			<MainText />
 			<SearchBar />
+			<MemoizedMainMap />
 		</Box>
 	);
 }
